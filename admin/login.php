a343e263b507efb4ecb6231b922ed016be634b6a
Ikhsan Nusaly <ikhsanskuy@Ikhsans-MacBook-Air.local>
2023-01-17 23:20:39 +0800

    Initiate

--- admin/login.php
+++ admin/login.php
@@ -60,13 +60,14 @@ include 'koneksi.php';
                                     <?php
                                         if(isset($_POST['login']))
                                         {
-                                            $ambil = $koneksi->query("SELECT * FROM admin WHERE username='$_POST[user]' AND password ='$_POST[pass]'");
+                                            $ambil = $koneksi->query("SELECT * FROM `admin` WHERE username='$_POST[user]' AND password ='$_POST[pass]'");
                                             $yangcocok = $ambil->num_rows;
                                             if ($yangcocok==1)
                                             {
                                                 $_SESSION['admin']=$ambil->fetch_assoc();
                                                 echo "<div class='alert alert-info'>Login Sukses</div>";
-                                                echo "<meta http-equiv='refresh' content='1;url=index.php'>";
+                                                // echo "<meta http-equiv='refresh' content='1;url=index.php'>";
+                                                header("location:index.php");
                                             }
                                             else
                                             {
