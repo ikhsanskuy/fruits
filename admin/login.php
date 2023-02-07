3b2956964a168a72f2a756007cef3f7a2330d5c0
Ikhsan Nusaly <ikhsanskuy@Ikhsans-MacBook-Air.local>
2023-02-07 17:08:55 +0800

    login

--- admin/login.php
+++ admin/login.php
@@ -34,7 +34,7 @@ include 'koneksi.php';
                   <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
                         <div class="panel panel-default">
                             <div class="panel-heading">
-                        <strong>Masuk untuk mendapatkan akses sebagaia admin</strong>  
+                        <strong>Masuk untuk mendapatkan akses sebagai admin</strong>  
                             </div>
                             <div class="panel-body">
                                 <form role="form" method="post">
