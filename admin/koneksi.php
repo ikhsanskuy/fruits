bd43dd027564c3d7e78ff88cd2ccd4ebc49bd152
Ikhsan Nusaly <ikhsanskuy@Ikhsans-MacBook-Air.local>
2023-02-07 16:52:49 +0800

    added more features

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,3 +1,4 @@
 <?php 
 
-    $koneksi = new mysqli ("localhost:8889", "root", "root", "fruit");
+    // $koneksi = new mysqli ("localhost", "root", "root", "fruit");
+    $koneksi = new mysqli ("localhost", "knwfruit_db", "Keren123@", "knwfruit_db");
--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,4 @@
-<?php
+<?php 
 
-$koneksi = new mysqli("localhost", "root", "root", "fruit");
+    // $koneksi = new mysqli ("localhost", "root", "root", "fruit");
+    $koneksi = new mysqli ("localhost", "knwfruit_db", "Keren123@", "knwfruit_db");
