45d54096731829e303da90f29cf3b503a2d59b19
Ikhsan Nusaly <ikhsanskuy@Ikhsans-MacBook-Air.local>
2023-02-07 21:52:55 +0800

    login

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,4 @@
 <?php 
 
-    $koneksi = new mysqli ("localhost", "root", "root", "fruit");
-    // $koneksi = new mysqli ("localhost", "knwfruit_db", "Keren123@", "knwfruit_db");
+    // $koneksi = new mysqli ("localhost", "root", "root", "fruit");
+    $koneksi = new mysqli ("localhost", "knwfruit_db", "Keren123@", "knwfruit_db");
